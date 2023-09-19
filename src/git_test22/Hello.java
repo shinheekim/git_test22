e361955b028426ed10465346f01d59c28cbf4b4f
CINI <shinhee1241@gmail.com>
2023-09-20 03:18:23 +0900

    최초 커밋

---
+++ src/git_test22/Hello.java
@@ -0,0 +1,7 @@
+package git_test22;
+
+public class Hello {
+	public void say() {
+		System.out.println("hello world");
+	}
+}
---
+++ src/git_test22/Main.java
@@ -0,0 +1,6 @@
+package git_test22;
+public class Main {
+ public static void main(String[] args) {
+ new Hello().say();
+ }
+}
